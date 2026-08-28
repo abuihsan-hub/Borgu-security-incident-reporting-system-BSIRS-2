884863ae06b1a2b5e982e98d5132052f845409d1
Abu Ihsan <muhammadsaniabubakar335@gmail.com>
2026-08-28 03:22:55 +0100

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,5 +1,10 @@
+// ===============================
+// BSIRS SUPABASE CONNECTION
+// ===============================
+
 const SUPABASE_URL = "https://tjvqasojbjpddhptxgtx.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "YOUR_SB_PUBLISHABLE_KEY";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_ykDbzp6fQmdWEy_penkpnw_E9Hrf6K_";
 
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
